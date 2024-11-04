028d29ecea7fe07b89fe62f759067e6c2194feb1
kTrzcinskii <kt.trzcinski@gmail.com>
2024-11-04 22:49:09 +0100

    Add slider for changing triangulation accuracy

--- src/triangle_mesh_filler.rs
+++ src/triangle_mesh_filler.rs
@@ -5,28 +5,45 @@ use anyhow::Result;
 use crate::{control_points::ControlPoints, drawer::Drawer, mesh::Mesh};
 
 pub struct TriangleMeshFiller {
+    controls_state: ControlsState,
     control_points: ControlPoints,
     mesh: Mesh,
 }
 
 impl TriangleMeshFiller {
     pub fn load_from_file<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let controls_state = ControlsState::default();
         let control_points = ControlPoints::load_from_file(path)?;
-        let mesh = Mesh::triangulation(&control_points, 30);
+        let mesh = Mesh::triangulation(&control_points, controls_state.triangulation_accuracy);
         Ok(Self {
             control_points,
             mesh,
+            controls_state: ControlsState::default(),
         })
+    }
+
+    fn recalculate_mesh(&mut self) {
+        let new_mesh = Mesh::triangulation(
+            &self.control_points,
+            self.controls_state.triangulation_accuracy,
+        );
+        self.mesh = new_mesh;
     }
 }
 
 impl eframe::App for TriangleMeshFiller {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        self.recalculate_mesh();
+
         egui::SidePanel::right("ControlsPanle")
             .resizable(false)
             .show(ctx, |ui| {
                 ui.heading("Controls");
                 ui.separator();
+                ui.add(
+                    egui::Slider::new(&mut self.controls_state.triangulation_accuracy, 10..=60)
+                        .text("Triangulation accuracy"),
+                )
             });
 
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -35,5 +52,17 @@ impl eframe::App for TriangleMeshFiller {
             Drawer::draw_control_points(painter, &screen_center, &self.control_points);
             Drawer::draw_mesh(painter, &screen_center, &self.mesh);
         });
+    }
+}
+
+struct ControlsState {
+    triangulation_accuracy: usize,
+}
+
+impl Default for ControlsState {
+    fn default() -> Self {
+        ControlsState {
+            triangulation_accuracy: 20,
+        }
     }
 }
